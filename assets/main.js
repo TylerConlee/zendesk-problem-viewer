b8fb8ceefa2152455b5e36f03381da4875b16b14
Tyler Conlee <tylerlconlee@gmail.com>
2018-05-29 09:21:30 -0700

    added new logo and showinfo/showerror funcs

--- assets/main.js
+++ assets/main.js
@@ -4,3 +4,25 @@ $(function() {
     var client = ZAFClient.init();
     client.invoke('resize', { width: '100%', height: '200px' });
 });
+
+function showInfo() {
+    var requester_data = {
+      'mrr': '32100',
+      'organization': ['org', 'org2'],
+    };
+  
+    var source = $("#requester-template").html();
+    var template = Handlebars.compile(source);
+    var html = template(requester_data);
+    $("#content").html(html);
+}
+function showError() {
+    var error_data = {
+      'status': 404,
+      'statusText': 'Not found'
+    };
+    var source = $("#error-template").html();
+    var template = Handlebars.compile(source);
+    var html = template(error_data);
+    $("#content").html(html);
+}  
