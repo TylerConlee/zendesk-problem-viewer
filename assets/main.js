c1838f21c282cb1fc784c73b8b9aa839cfb79b6b
Tyler Conlee <tylerlconlee@gmail.com>
2018-05-30 14:47:40 -0700

    handling promises

--- assets/main.js
+++ assets/main.js
@@ -1,28 +1,88 @@
+var orgs = [];
 $(function() {
     // Initialise the Zendesk JavaScript API client
     // https://developer.zendesk.com/apps/docs/apps-v2
     var client = ZAFClient.init();
-    client.invoke('resize', { width: '100%', height: '200px' });
+    client.get('ticket.type').then(function(data) {
+      	if (data['ticket.type'] === 'problem') {
+        	client.get('ticket.id').then(function(data) {
+          		var ticket = data['ticket.id'];
+          		getIncidents(ticket)
+      		},
+      		function(response) {
+        		showError(response);
+      		});
+      	}
+    });
 });
 
-function showInfo() {
-    var requester_data = {
-      'mrr': '32100',
-      'organization': ['org', 'org2'],
-    };
-  
-    var source = $("#requester-template").html();
-    var template = Handlebars.compile(source);
-    var html = template(requester_data);
-    $("#content").html(html);
+function getIncidents(ticket){
+  	var client = ZAFClient.init();
+  	var fetchSelf = {
+		url: '/api/v2/tickets/' + ticket + '/incidents.json',
+		type: 'GET',
+		dataType: 'json'
+  	};
+  	client.request(fetchSelf).then(function(data) {
+    	data.tickets.forEach(function(ticket){
+			var org = new Object();
+			n = getOrgName(ticket.organization_id).then(function(name){return name}).then(name => {
+				org.name = name;
+				console.log(org.name)
+				var i = ticket.custom_fields.findIndex( (el) => el.id === 80756047);
+				org.mrr = ticket.custom_fields[i].value;
+				orgs.push(org)
+				return name
+			});
+		});
+		return orgs
+  	}).finally(function(){
+		setTimeout(function(){
+		showInfo(orgs)
+	});
+  	},function(response) {
+    	showError(response);
+  	});
 }
+
+function getOrgName(id){
+  	var client = ZAFClient.init();
+  	var fetchSelf = {
+    	url: '/api/v2/organizations/' + id + '.json',
+    	type: 'GET',
+    	dataType: 'json'
+  	};
+  	var name = client.request(fetchSelf).then(function(data) {
+    	return data.organization.name
+	});
+	return name;
+}
+
+
+function showInfo(data) {
+	  var total = 0;
+	  console.log(data)
+  	data.forEach(function(m) {
+    	total = parseInt(m.mrr)+total;
+  	});
+  	var templatedata = {
+    	'data': data,
+    	'totalmrr': total,
+  	};
+  	console.log(templatedata)
+  	var source = $("#requester-template").html();
+  	var template = Handlebars.compile(source);
+  	var html = template(templatedata);
+  	$("#content").html(html);
+}
+
 function showError() {
-    var error_data = {
-      'status': 404,
-      'statusText': 'Not found'
-    };
-    var source = $("#error-template").html();
-    var template = Handlebars.compile(source);
-    var html = template(error_data);
-    $("#content").html(html);
-}  
+  	var error_data = {
+    	'status': response.status,
+    	'statusText': response.statusText
+  	};
+  	var source = $("#error-template").html();
+  	var template = Handlebars.compile(source);
+  	var html = template(error_data);
+  	$("#content").html(html);
+}
